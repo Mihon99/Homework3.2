869e20ca7a1ea9329b4e7e2d92de3ee63378ad17
reaper <reaper>
2022-10-05 20:57:22 +0300

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите  число: ");
+string myString = Console.ReadLine().ToString();
+int b = int.Parse(myString);
+if (b / 100 > 0)
+{
+    Console.WriteLine(myString[2]);
+}
+else
+{
+    Console.WriteLine("Третьего цифры в числе нет!");
+}
