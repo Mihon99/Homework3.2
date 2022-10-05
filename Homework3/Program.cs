c5d75b1755e3f6e3cf098fbaa7cc70a2e40848fa
reaper <reaper>
2022-10-05 21:15:08 +0300

    Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите  число: ");
+int b = int.Parse(Console.ReadLine());
+if(b<1)
+{
+    Console.WriteLine("Ошибка");
+}
+else if(b > 7)
+{
+    Console.WriteLine("Ошибка");
+}
+else if(b == 6)
+{
+    Console.WriteLine("Ура, выходные начались, чилим!");
+}
+else if(b == 7)
+{
+    Console.WriteLine("Последний день выходных, отдохни как следует!");
+}
+else if(b >= 1 && b <= 5)
+{
+    Console.WriteLine("Будние дни, пашем как папа Карло!");
+}
