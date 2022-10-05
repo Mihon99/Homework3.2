7aada74d051b30819a61c4348a5b204a4039c29b
reaper <reaper>
2022-10-05 20:41:24 +0300

    Homework1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,4 +1,3 @@
 ﻿Console.Write("Введите  число: ");
-string number1 = Console.ReadLine();
-string myString = number1.ToString();
+string myString = Console.ReadLine().ToString();
 Console.WriteLine(myString[1]);
