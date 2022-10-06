bda98296544a10d675bf3abdee1794a11a29b59f
reaper <reaper>
2022-10-06 19:28:31 +0300

    Homework2

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -5,23 +5,23 @@ if (b < 0)
 {
     b = -1 * b;
     if (b / 100 > 0)
-{
-    string s = b.ToString();
-    Console.WriteLine(s[2]);
-}
-else
-{
-    Console.WriteLine("Третьего цифры в числе нет!");
-}
+    {
+        string s = b.ToString();
+        Console.WriteLine(s[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьего цифры в числе нет!");
+    }
 }
 else
 {
     if (b / 100 > 0)
-{
-    Console.WriteLine(myString[2]);
-}
-else
-{
-    Console.WriteLine("Третьего цифры в числе нет!");
-}
+    {
+        Console.WriteLine(myString[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьего цифры в числе нет!");
+    }
 }
