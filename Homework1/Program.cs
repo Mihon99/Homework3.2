38fd713fe9126893fcadf1204daa2bca3bb46bc3
reaper <reaper>
2022-10-06 19:24:49 +0300

    Homework1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.Write("Введите  число: ");
 string myString = Console.ReadLine().ToString();
-Console.WriteLine(myString[1]);
+int b = int.Parse(myString);
+if (b < 0)
+{
+    b = -1 * b;
+    if (b / 100 > 0)
+{
+    string s = b.ToString();
+    Console.WriteLine(s[1]);
+}
+}
+else
+{
+   Console.WriteLine(myString[1]);
+}
