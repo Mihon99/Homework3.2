dd7980dccd6b01cc6857aecf0e2de4c96721b6b2
reaper <reaper>
2022-10-06 19:20:19 +0300

    Homework2

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,11 +1,27 @@
 ﻿Console.Write("Введите  число: ");
 string myString = Console.ReadLine().ToString();
 int b = int.Parse(myString);
-if (b / 100 > 0)
+if (b < 0)
+{
+    b = -1 * b;
+    if (b / 100 > 0)
+{
+    string s = b.ToString();
+    Console.WriteLine(s[2]);
+}
+else
+{
+    Console.WriteLine("Третьего цифры в числе нет!");
+}
+}
+else
+{
+    if (b / 100 > 0)
 {
     Console.WriteLine(myString[2]);
 }
 else
 {
     Console.WriteLine("Третьего цифры в числе нет!");
+}
 }
